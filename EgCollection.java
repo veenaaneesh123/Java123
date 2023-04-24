d591d25dc441c1435023a4b2dc24b63cf1ba9f5c
veenaaneesh123 <me.veenaneesh.ar@gmail.com>
2023-04-24 20:31:32 +0530

    added a java class

---
+++ EgCollection.java
@@ -0,0 +1,43 @@
+package Collection;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class EgCollection {
+	public static void main(String arg[])
+	{
+		//List<String> list =new ArrayList<String>();
+		ArrayList<Integer> arrlist=new ArrayList<Integer>();
+		
+	
+
+	arrlist.add(123);
+	arrlist.add(123);
+	arrlist.add(456);
+	arrlist.add(234);
+	Iterator<Integer> itr=arrlist.iterator();  
+	while(itr.hasNext())
+	{  
+	System.out.println(itr.next());  
+	}
+	//System.out.println("List"+arrlist);
+	
+	
+	ArrayList<String> list=new ArrayList<String>();
+	list.add("Rafgb"); 
+	list.add("Vifgg");  
+	list.add("Rafff");  
+	list.add("Ahfffff");
+	
+	Iterator<String> itr1=list.iterator();  
+	while(itr1.hasNext())
+	{  
+	System.out.println(itr1.next());  
+	}
+	//System.out.println(list);
+	//System.out.println(arrlist.);
+	}
+	
+}
+
